d1b06076ee2f962c9084e28cef233b3039148674
Christian <brewer.christiang@gmail.com>
2024-02-13 10:33:38 -0500

    completes secret project

--- section_28/5.6+Secrets+Project/5.6 Secrets Project/index.js
+++ section_28/5.6+Secrets+Project/5.6 Secrets Project/index.js
@@ -7,8 +7,21 @@ import axios from "axios";
 const app = express();
 const PORT = 3000;
 // 3. Use the public folder for static files.
+app.use(express.static("public"));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get("https://secrets-api.appbrewery.com/random");
+        console.log(result);
+        const secret = result.data.secret;
+        const user = result.data.username;
+        res.render("index.ejs", {secret: secret, user: user});
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+})
 
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
